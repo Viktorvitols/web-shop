66b78f0cef4b3d625cef55048185b4fe8a937da5
Viktorvitols <viktorvitols@gmail.com>
2020-10-18 21:00:23 +0300

    Session added

--- src/main/java/com/app/controllers/LoginController.java
+++ src/main/java/com/app/controllers/LoginController.java
@@ -2,6 +2,7 @@ package com.app.controllers;
 
 import com.app.model.Login;
 import com.app.services.LoginService;
+import com.app.session.CurrentUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +16,17 @@ import java.sql.SQLException;
 public class LoginController {
 
     @Autowired
-    LoginService loginService;
+    private LoginService loginService;
+
+    @Autowired
+    private CurrentUser currentUser;
 
     @GetMapping("/login")
     public String getLoginForm(Model model) {
+        if (currentUser.getId() != null) {
+            model.addAttribute("id", currentUser.getId());
+            return "login_response";
+        }
         model.addAttribute("login", new Login());
         return "login";
     }
@@ -27,9 +35,10 @@ public class LoginController {
     public String loginUser(@ModelAttribute Login login, Model model) throws SQLException {
         Integer userId = loginService.getUserId(login);
         if (userId != null) {
+            currentUser.setId(userId);
             model.addAttribute("id", userId);
-            return "login_response";
+//            return "login_response";
         }
-        return "login";
+        return "redirect:/login";
     }
 }
