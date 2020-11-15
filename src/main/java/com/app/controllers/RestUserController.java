dae6295d2043e33fb72fb2bb13d7f2a42a6c51c6
Viktorvitols <viktorvitols@gmail.com>
2020-11-15 15:32:11 +0200

    Date refactor Rest API - get users + cross origin

--- src/main/java/com/app/controllers/RestUserController.java
+++ src/main/java/com/app/controllers/RestUserController.java
@@ -3,6 +3,7 @@ package com.app.controllers;
 import com.app.model.User;
 import com.app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,9 @@ public class RestUserController {
     @Autowired
     UserService userService;
 
+    @CrossOrigin
     @GetMapping("/getUsers")
     public List<User> getUsers() {
         return userService.getAllUsers();
     }
-
 }
