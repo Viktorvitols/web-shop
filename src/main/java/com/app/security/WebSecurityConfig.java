f6f84d0df6f97a03495fcd877447ec85bd05807b
Viktorvitols <viktorvitols@gmail.com>
2021-01-24 21:01:33 +0200

    Add roles (enums) + thymeleaf allUsers

--- src/main/java/com/app/security/WebSecurityConfig.java
+++ src/main/java/com/app/security/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
-                    .antMatchers("/sechome", "/").permitAll()
+                    .antMatchers("/sechome", "/", "/allUsers").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
